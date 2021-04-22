48fbf1c91ad2a81e385eca25a8748b5abc960b7f
MrAugu <39545629+MrAugu@users.noreply.github.com>
2021-04-22 17:01:07 +0300

    Switching from cookie to authorization header

--- src/plugins/authentication.js
+++ src/plugins/authentication.js
@@ -5,11 +5,14 @@ const jwt = require("jsonwebtoken");
 
 async function plugin (fastify, options) { // eslint-disable-line no-unused-vars 
   fastify.decorate("authenticate", async function (request, response) { // eslint-disable-line no-unused-vars
-    if (!request.cookies.token) return response.code(401).send(httpCodes["401"]());
+    if (!request.headers.authorization) return response.code(401).send(httpCodes["401"]());
+    const [bearer, token] = request.headers.authorization.split(" ");
+    if (!bearer || !token || bearer !== "Bearer") return response.code(401).send(httpCodes["401"]());
     try {
-      const verifiedToken = jwt.verify(request.cookies.token, process.env.KEY);
+      const verifiedToken = jwt.verify(token, process.env.KEY);
       if (verifiedToken.expires <= Date.now()) response.code(401).send(httpCodes["401"]("Token has expired."));
       request.user = verifiedToken;
+      request.user.token = token;
     } catch (e) {
       return response.code(401).send(httpCodes["401"]());
     }
--- src/routes/session.js
+++ src/routes/session.js
@@ -26,15 +26,10 @@ const routes = async (fastify) => {
   fastify.post("/login", async (request, response) => {
     response.type("application/json");
 
-    if (request.cookies.token && !request.query.ignoreCookie) {
-      try {
-        const parsedToken = jwt.verify(request.cookies.token, process.env.KEY);
-        if (parsedToken.username && parsedToken.expires > Date.now()) return  response.code(200).send(httpCodes["DATA_200"]({
-          token: request.cookies.token
-        }, "Used token cookie."));
-      } catch (error) {
-        // eslint-disable-line no-empty
-      }
+    if (request.user) {
+      return  response.code(200).send(httpCodes["DATA_200"]({
+        token: request.user.token
+      }, "Used the authorization header."));
     }
 
     if (!request.body || !request.body.username || !request.body.password) return response.code(400).send(httpCodes["400"]());
@@ -48,25 +43,26 @@ const routes = async (fastify) => {
 
     const token = jwt.sign({
       username: user.username,
-      expires: (Date.now() + (1000 * 60 * 60 * 24 * 2)),
+      expires: (Date.now() + (1000 * 60 * 60 * 0.5)),
       id: user._id,
       hash: user.password,
       displayName: user.displayName,
       color: user.color
     }, process.env.KEY);
 
-    response.setCookie("token", token);
+    const refreshToken = jwt.sign({
+      id: user._id
+    }, process.env.REFRESH_KEY);
 
     return response.code(200).send(httpCodes["DATA_200"]({
-      token
+      token,
+      refreshToken
     }));
   });
 
   fastify.get("/logout", {
     preValidation: [fastify.authenticate]
   }, async (request, response) => {
-    response.clearCookie("token");
-
     return response.code(204).send();
   });
 };
